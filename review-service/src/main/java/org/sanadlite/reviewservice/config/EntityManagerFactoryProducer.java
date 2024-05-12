a98cffe4864b64fdace6b2b72721871a10d98c38
greatyassoo <63516733+greatyassoo@users.noreply.github.com>
2024-05-13 02:36:21 +0300

    enable entity manager cdi

---
+++ review-service/src/main/java/org/sanadlite/reviewservice/config/EntityManagerFactoryProducer.java
@@ -0,0 +1,19 @@
+package org.sanadlite.reviewservice.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EntityManagerFactoryProducer {
+    @Produces
+    @ApplicationScoped
+    public EntityManagerFactory create() {
+        return Persistence.createEntityManagerFactory("default");
+    }
+
+    public void destroy(@Disposes EntityManagerFactory factory) {
+        factory.close();
+    }
+}
--- review-service/src/main/java/org/sanadlite/reviewservice/service/ReviewService.java
+++ review-service/src/main/java/org/sanadlite/reviewservice/service/ReviewService.java
@@ -1,11 +1,20 @@
 package org.sanadlite.reviewservice.service;
 
-import jakarta.ejb.EJB;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import org.sanadlite.reviewservice.model.Review;
 
 @ApplicationScoped
 public class ReviewService {
+    @Inject
+    private EntityManager entityManager;
     public String test() {
-        return "lol";
+        Review review = new Review(2L, 2L, 5.0, "review test 2");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(review);
+        entityManager.getTransaction().commit();
+        return "Hello, World!";
     }
 }
