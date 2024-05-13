735ccc19b6a86f3b78d7fb9418045b60803beab0
greatyassoo <63516733+greatyassoo@users.noreply.github.com>
2024-05-14 02:52:00 +0300

    init course-enrollment-service

---
+++ course-enrollment-service/src/main/java/org/sanadlite/courseenrollmentservice/CourseEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sanadlite.courseenrollmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEnrollmentServiceApplication.class, args);
+    }
+
+}
---
+++ course-enrollment-service/src/test/java/org/sanadlite/courseenrollmentservice/CourseEnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanadlite.courseenrollmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
